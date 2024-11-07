7cafe4f4cf73657bb33a24c5cd907e921a3425dc
德华 刘 <gavinprior5@gmail.com>
2024-11-07 19:41:39 +0000

    adjust width of navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,14 +3,14 @@ import { PiMoonStarsLight } from "react-icons/pi";
 
 const Navbar = () => {
   return (
-    <header className="max-w-[900px] sticky top-0 z-20 bg-background/75 py-6 backdrop-blur-sm px-8 bg-background text-foreground">
+    <header className="max-w-3xl w-full sticky top-0 z-20 bg-background/75 py-6 backdrop-blur-sm px-8 sm:px-0 bg-background text-foreground">
       <nav className="flex justify-between items-center">
         <ul className="flex gap-4 sm:gap-8">
           <li>Home</li>
           <li>Projects</li>
           <li>Contact</li>
         </ul>
-        <div className="flex gap-0 sm:ml-4">
+        <div className="flex gap-0 sm:ml-8">
           <button className="h-9 w-9 inline-flex justify-center items-center">
             <PiMoonStarsLight size={24} className="text-indigo-500"/>
           </button>
