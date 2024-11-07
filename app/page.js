61bf099da2815c18c327c5e597ba8e4090809a2d
德华 刘 <gavinprior5@gmail.com>
2024-11-07 17:59:12 +0000

    add flex to main

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div>
-      <main className="max-w-[900px] min-h-screen bg-background mx-auto" >
+      <main className="max-w-[900px] min-h-screen bg-background mx-auto flex flex-col" >
         Hello, world!
       </main>
     </div>
