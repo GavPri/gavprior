4367417705c113a1b35eeb4488ec6fdfa7857304
德华 刘 <gavinprior5@gmail.com>
2024-11-07 19:15:16 +0000

    create styled navbar
    no functionality yet for routing

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { PiMoonStarsLight } from "react-icons/pi";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="max-w-[900px] sticky top-0 z-20 bg-background/75 py-6 backdrop-blur-sm px-8 bg-background text-foreground">
+      <nav className="flex justify-between items-center">
+        <ul className="flex gap-4 sm:gap-8">
+          <li>Home</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+        <div className="flex gap-0 sm:ml-4">
+          <button className="h-9 w-9 inline-flex justify-center items-center">
+            <PiMoonStarsLight size={24} className="text-indigo-500"/>
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
