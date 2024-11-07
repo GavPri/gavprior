b39622d738da2c1ac780dc63ea2a72ca361b0988
德华 刘 <gavinprior5@gmail.com>
2024-11-07 19:26:10 +0000

    adjust height of container

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="max-w-[900px] min-h-screen bg-background flex flex-col">
+    <div className="max-w-[900px] min-h-[calc(100vh-80px)] bg-background flex flex-col">
       <main>Hello, world!</main>
     </div>
   );
