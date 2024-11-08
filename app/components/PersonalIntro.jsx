0e234da71b29a43177a6f10daa7624965c800b5b
德华 刘 <gavinprior5@gmail.com>
2024-11-08 13:44:59 +0000

    add image to component

--- app/components/PersonalIntro.jsx
+++ app/components/PersonalIntro.jsx
@@ -3,7 +3,18 @@ import React from "react";
 import Image from "next/image";
 
 const PersonalIntro = () => {
-  return <section className="flex flex-col items-start mt-8"></section>;
+  return (
+    <section className="flex flex-col items-start mt-8 ">
+      <Image
+        src="/images/hero.jpg"
+        alt="Profile Picture"
+        width={200}
+        height={200}
+        priority
+        className="rounded-lg"
+      />
+    </section>
+  );
 };
 
 export default PersonalIntro;
