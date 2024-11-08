6112737ca57100ca379a0d375b66fbb8aaacb092
德华 刘 <gavinprior5@gmail.com>
2024-11-08 14:33:55 +0000

    import and add personal intro comp.

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
+import PersonalIntro from "./components/PersonalIntro";
+
 export default function Home() {
   return (
       <main className="grow">
-        <article className="flex flex-col mt-8 gap-16 pb-16"></article>
+        <article className="flex flex-col mt-8 gap-16 pb-16">
+          <PersonalIntro />
+        </article>
       </main>
   );
 }
