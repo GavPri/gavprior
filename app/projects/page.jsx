2461b25a59c83a65ad028f1303e2af037f94ba2e
德华 刘 <gavinprior5@gmail.com>
2024-11-08 14:56:42 +0000

    add page.jsx file with header

---
+++ app/projects/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <section className="mt-8 flex flex-col gap-8 pb-16">
+      <h1 className="text-4xl text-foreground tracking-wide font-bold">
+        My Projects
+      </h1>
+    </section>
+  );
+}
+
+export default page
