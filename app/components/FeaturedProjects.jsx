62ec5b21bb0d1463eba9e0594311c368c95ba26c
德华 刘 <gavinprior5@gmail.com>
2024-11-12 17:49:10 +0000

    add flex to section

--- app/components/FeaturedProjects.jsx
+++ app/components/FeaturedProjects.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const FeaturedProjects = () => {
-  return (
-    <section>FeaturedProjects</section>
-  )
-}
+  return <section className="flex flex-col gap-8">FeaturedProjects</section>;
+};
 
-export default FeaturedProjects
+export default FeaturedProjects;
