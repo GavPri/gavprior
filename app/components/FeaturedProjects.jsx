2f4b73e47edbc468cc2bff41a6510085a0e8020b
德华 刘 <gavinprior5@gmail.com>
2024-11-12 17:56:22 +0000

    alter text size of header

--- app/components/FeaturedProjects.jsx
+++ app/components/FeaturedProjects.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Link from "next/link";
 import { IoIosArrowRoundForward } from "react-icons/io";
+
 const FeaturedProjects = () => {
   return (
     <section className="flex flex-col gap-8">
       <div className="flex justify-between">
-        <h2 className="font-bold text-lg text-foreground">Featured Projects</h2>
-        <Link href="/projects" className="inline-flex items-center font-thin">
+        <h2 className="font-bold text-xl text-foreground">Featured Projects</h2>
+        <Link href="/projects" className="inline-flex items-center font-thin text-mutedForeground">
           view more <IoIosArrowRoundForward size={25} />
         </Link>
       </div>
