eeefac7bd9ad5ed5b59683478842f02f9439c544
德华 刘 <gavinprior5@gmail.com>
2024-11-12 15:40:17 +0000

    add third project to array

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -17,6 +17,14 @@ const projectData = [
     tags: ["React.js", "TailwindCSS"],
     githubLink: "https://github.com/GavPri/twitter_front_end",
   },
+  {
+    title: "Not Twitter Restful API",
+    description:
+      "A Django RESTful API for a social media site, handling user authentication, posts, comments, likes, and user follow functionality, providing a secure and scalable backend for seamless interaction.",
+    imageSrc: "/images/notTwitterAPIBanner.jpg",
+    tags: ["Python", "Django"],
+    githubLink: "https://github.com/GavPri/Twitter_Clone_API",
+  },
 ];
 
 const page = () => {
