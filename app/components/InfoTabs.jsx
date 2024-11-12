bf72e20d71ae23da658ed417ab4436a9af695f8d
德华 刘 <gavinprior5@gmail.com>
2024-11-12 16:21:10 +0000

    add styles to tab headers

--- app/components/InfoTabs.jsx
+++ app/components/InfoTabs.jsx
@@ -1,10 +1,31 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
 
 const InfoTabs = () => {
   const [tab, setTab] = useState("education");
-  return <div>InfoTabs</div>;
+  return (
+    <div className="h-9 items-center justify-center rounded-lg bg-muted p-1 text-mutedForeground mb-2 grid grid-cols-2">
+      <button
+        className={
+          tab === "work"
+            ? "bg-background text-foreground rounded-lg"
+            : `opacity-50 inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50`
+        }
+      >
+        Work
+      </button>
+      <button
+        className={
+          tab === "education"
+            ? "bg-background text-foreground rounded-lg shadow-md"
+            : `opacity-50 inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50`
+        }
+      >
+        Education
+      </button>
+    </div>
+  );
 };
 
 export default InfoTabs;
