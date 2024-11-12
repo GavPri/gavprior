bf7ea4cf3d6205e557fff54f2446bf34b27d8760
德华 刘 <gavinprior5@gmail.com>
2024-11-12 18:16:46 +0000

    add form with inner divs for inputs

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -4,6 +4,13 @@ const page = () => {
   return (
     <section className="mt-8 flex flex-col gap-8 pb-16">
       <h1 className="text-2xl text-foreground font-bold">Contact Me</h1>
+      <form>
+        <div className="grid grid-cols-1 gap-2 sm:grid-cols-2">
+          <div className="h-16"></div>
+          <div className="h-16"></div>
+          <div className="h-32"></div>
+        </div>
+      </form>
     </section>
   );
 };
