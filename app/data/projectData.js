ffe69ca348b00a2eb9430353a8188aba48237d45
德华 刘 <gavinprior5@gmail.com>
2024-11-12 17:43:26 +0000

    create project data file for projects

---
+++ app/data/projectData.js
@@ -0,0 +1,29 @@
+// projectData.js
+const projectData = [
+  {
+    title: "GoWithSoph",
+    description:
+      "A mern stack travel blog built for my girlfriend, Sophie. Built with user authentication which allows users to like and comment on posts.",
+    imageSrc: "/images/gowithsophsnippet.jpg", // Path to the project image
+    tags: ["Node.js", "Express.js", "React.js", "TailwindCSS", "MongoDB"],
+    githubLink: "https://github.com/GavPri/go-with-soph-mern",
+  },
+  {
+    title: "Not Twitter Front End",
+    description:
+      "A simple social media site built with React.js and TailwindCSS, connecting to a Django RESTful API for handling user authentication, posts, comments, likes, and following users.",
+    imageSrc: "/images/notTwitterBanner.jpg", // Path to the project image
+    tags: ["React.js", "TailwindCSS"],
+    githubLink: "https://github.com/GavPri/twitter_front_end",
+  },
+  {
+    title: "Not Twitter Restful API",
+    description:
+      "A Django RESTful API for a social media site, handling user authentication, posts, comments, likes, and user follow functionality, providing a secure and scalable backend for seamless interaction.",
+    imageSrc: "/images/notTwitterAPIBanner.jpg",
+    tags: ["Python", "Django"],
+    githubLink: "https://github.com/GavPri/Twitter_Clone_API",
+  },
+];
+
+export default projectData;
