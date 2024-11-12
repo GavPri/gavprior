1e883d2df5ea52fd1aea764b8d8ba7955b2f53b1
德华 刘 <gavinprior5@gmail.com>
2024-11-12 18:03:46 +0000

    import data and card for map function

--- app/components/FeaturedProjects.jsx
+++ app/components/FeaturedProjects.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import Link from "next/link";
 import { IoIosArrowRoundForward } from "react-icons/io";
+import projectData from "../data/projectData";
+import ProjectCard from "../projects/components/ProjectCard";
 
 const FeaturedProjects = () => {
   return (
     <section className="flex flex-col gap-8">
       <div className="flex justify-between">
         <h2 className="font-bold text-xl text-foreground">Featured Projects</h2>
-        <Link href="/projects" className="inline-flex items-center font-thin text-mutedForeground">
+        <Link
+          href="/projects"
+          className="inline-flex items-center font-thin text-mutedForeground"
+        >
           view more <IoIosArrowRoundForward size={25} />
         </Link>
+      </div>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+        {projectData.slice(0, 2).map((project, index) => (
+          <ProjectCard
+            key={index}
+            title={project.title}
+            description={project.description}
+            imageSrc={project.imageSrc}
+            tags={project.tags}
+            githubLink={project.githubLink}
+          />
+        ))}
       </div>
     </section>
   );
