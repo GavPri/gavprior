37878b7668dcef678a691075ca01484c9877d832
德华 刘 <gavinprior5@gmail.com>
2024-11-12 17:54:17 +0000

    add heading for section

--- app/components/FeaturedProjects.jsx
+++ app/components/FeaturedProjects.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-
+import Link from "next/link";
+import { IoIosArrowRoundForward } from "react-icons/io";
 const FeaturedProjects = () => {
-  return <section className="flex flex-col gap-8">FeaturedProjects</section>;
+  return (
+    <section className="flex flex-col gap-8">
+      <div className="flex justify-between">
+        <h2 className="font-bold text-lg text-foreground">Featured Projects</h2>
+        <Link href="/projects" className="inline-flex items-center font-thin">
+          view more <IoIosArrowRoundForward size={25} />
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default FeaturedProjects;
