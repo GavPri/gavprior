160cf4b12f3b550513faeda0ed58dbb24674627e
德华 刘 <gavinprior5@gmail.com>
2024-11-12 17:46:01 +0000

    add shadow to work title tab

--- app/components/InfoTabs.jsx
+++ app/components/InfoTabs.jsx
@@ -17,7 +17,7 @@ const InfoTabs = () => {
           onClick={() => handleTabChange("work")}
           className={
             tab === "work"
-              ? "bg-background text-foreground rounded-lg"
+              ? "bg-background text-foreground rounded-lg shadow-md"
               : `opacity-50 inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50`
           }
         >
