73027d888ee95b1676afccf5dff151469198138e
德华 刘 <gavinprior5@gmail.com>
2024-11-12 16:30:07 +0000

    add click handler on tab headers

--- app/components/InfoTabs.jsx
+++ app/components/InfoTabs.jsx
@@ -4,9 +4,14 @@ import React, { useState } from "react";
 
 const InfoTabs = () => {
   const [tab, setTab] = useState("education");
+
+  const handleTabChange = (tab) => {
+    setTab(tab);
+  };
   return (
     <div className="h-9 items-center justify-center rounded-lg bg-muted p-1 text-mutedForeground mb-2 grid grid-cols-2">
       <button
+        onClick={() => handleTabChange("work")}
         className={
           tab === "work"
             ? "bg-background text-foreground rounded-lg"
@@ -16,6 +21,7 @@ const InfoTabs = () => {
         Work
       </button>
       <button
+        onClick={() => handleTabChange("education")}
         className={
           tab === "education"
             ? "bg-background text-foreground rounded-lg shadow-md"
