43f9c5f7639ec9438da2c68b323a6bc27775042a
德华 刘 <gavinprior5@gmail.com>
2024-11-12 18:14:42 +0000

    add heading for file

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <section className="mt-8 flex flex-col gap-8 pb-16">
+      <h1 className="text-2xl text-foreground font-bold">Contact Me</h1>
+    </section>
+  );
+};
 
-export default page
+export default page;
