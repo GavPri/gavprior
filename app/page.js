74711de65961b799e6fa49dc9279b87859d125a4
德华 刘 <gavinprior5@gmail.com>
2024-11-12 16:30:43 +0000

    adjust width of main

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
+import InfoTabs from "./components/InfoTabs";
 import PersonalIntro from "./components/PersonalIntro";
 
 export default function Home() {
   return (
-      <main className="grow">
+      <main className="grow w-full">
         <article className="flex flex-col mt-8 gap-16 pb-16">
           <PersonalIntro />
+          <InfoTabs />
         </article>
       </main>
   );
