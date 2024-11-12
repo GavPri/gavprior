5fb53222d61438ad508fe5c5b7b959d2df4ce1af
德华 刘 <gavinprior5@gmail.com>
2024-11-12 18:04:17 +0000

    import featured porjects

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
+import FeaturedProjects from "./components/FeaturedProjects";
 import InfoTabs from "./components/InfoTabs";
 import PersonalIntro from "./components/PersonalIntro";
 
 export default function Home() {
   return (
-      <main className="grow w-full">
-        <article className="flex flex-col mt-8 gap-16 pb-16">
-          <PersonalIntro />
-          <InfoTabs />
-        </article>
-      </main>
+    <main className="grow w-full">
+      <article className="flex flex-col mt-8 gap-16 pb-16">
+        <PersonalIntro />
+        <InfoTabs />
+        <FeaturedProjects />
+      </article>
+    </main>
   );
 }
