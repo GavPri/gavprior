067c8d902ee3454bca0090513eb2fe95b13f6cde
德华 刘 <gavinprior5@gmail.com>
2024-11-18 14:00:01 +0000

    create new component

---
+++ app/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <section className='w-full h-32 flex justify-between'>
+        <p></p>
+        <ul></ul>
+    </section>
+  )
+}
