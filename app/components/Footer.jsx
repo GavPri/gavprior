97fc311fc3734171f4bd8f65ca096fa695241084
德华 刘 <gavinprior5@gmail.com>
2024-11-18 15:17:55 +0000

    add functional components as footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,37 +4,41 @@ import { MdOutlineCopyright } from "react-icons/md";
 import { SlSocialLinkedin } from "react-icons/sl";
 import { TfiEmail } from "react-icons/tfi";
 
-export default function () {
+import React from 'react'
+
+const Footer = () => {
   return (
     <section className="w-full h-16 mb-4 flex justify-between items-center">
       <p className="text-mutedForeground inline-flex items-center">
         <span>
-          <MdOutlineCopyright className="mr-2"/>
+          <MdOutlineCopyright className="mr-2" />
         </span>{" "}
         gavprior.com{" "}
       </p>
       <ul className="flex gap-6">
-            <a
-              target="_blank"
-              href="https://github.com/GavPri"
-              className="text-mutedForeground hover:text-foreground"
-            >
-              <FiGithub size={20} />
-            </a>
-            <a
-              target="_blank"
-              href="https://linkedin.com/in/gavpdev"
-              className="text-mutedForeground hover:text-foreground"
-            >
-              <SlSocialLinkedin size={20} />
-            </a>
-            <a
-              href="mailto:gavinprior13@example.com"
-              className="text-mutedForeground hover:text-foreground"
-            >
-              <TfiEmail size={20} />
-            </a>
+        <a
+          target="_blank"
+          href="https://github.com/GavPri"
+          className="text-mutedForeground hover:text-foreground"
+        >
+          <FiGithub size={20} />
+        </a>
+        <a
+          target="_blank"
+          href="https://linkedin.com/in/gavpdev"
+          className="text-mutedForeground hover:text-foreground"
+        >
+          <SlSocialLinkedin size={20} />
+        </a>
+        <a
+          href="mailto:gavinprior13@example.com"
+          className="text-mutedForeground hover:text-foreground"
+        >
+          <TfiEmail size={20} />
+        </a>
       </ul>
     </section>
   );
 }
+
+export default Footer
