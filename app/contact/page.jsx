93fd5a3d42b4d6540057df07ec9cc9833180e09b
德华 刘 <gavinprior5@gmail.com>
2024-11-18 12:00:11 +0000

    add name to input, style input text
    remove message from destructuring

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useState } from "react";
 import { IoPaperPlaneOutline } from "react-icons/io5";
 
@@ -13,7 +15,7 @@ const page = () => {
   });
 
   const hanldeChange = (e) => {
-    const { name, value, message } = e.target;
+    const { name, value } = e.target;
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
   return (
@@ -23,19 +25,28 @@ const page = () => {
         <div className="grid grid-cols-1 gap-2 sm:grid-cols-2">
           <div className="h-16">
             <input
-              className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-mutedForeground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+              value={formData.name}
+              onChange={hanldeChange}
+              name="name"
+              className="text-mutedForeground flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-mutedForeground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
               placeholder="Name"
             />
           </div>
           <div className="h-16">
             <input
-              className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-mutedForeground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+              value={formData.email}
+              name="email"
+              onChange={hanldeChange}
+              className="text-foreground flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-mutedForeground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
               placeholder="Email"
             />
           </div>
           <div className="h-32 sm:col-span-2">
             <textarea
-              className="flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-mutedForeground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 resize-none"
+              value={formData.message}
+              name="message"
+              onChange={hanldeChange}
+              className="text-foreground flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-mutedForeground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 resize-none"
               placeholder="Leave feedback, career oppurtunities or just say hello!"
               rows={6}
             />
