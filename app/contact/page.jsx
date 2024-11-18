ddfead1282f2203edf4341092eab2b0cc706d0a9
德华 刘 <gavinprior5@gmail.com>
2024-11-18 11:40:58 +0000

    destructure for values for inputs

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,20 +1,20 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { IoPaperPlaneOutline } from "react-icons/io5";
-
 
 // TODO -- Create state for the form.
 // TODO -- add handle change for form input fields.
 // TODO -- add a handle submit for the form to send to my email.
 // TODO -- redirect users to the home page with toast notification.
 const page = () => {
-
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  })
+    name: "",
+    email: "",
+    message: "",
+  });
 
-  const hanldeChange = (e) => {}
+  const hanldeChange = (e) => {
+    const { name, value } = e.target;
+  };
   return (
     <section className="mt-8 flex flex-col gap-8 pb-16 w-full">
       <h1 className="text-2xl text-foreground font-bold">Contact Me</h1>
