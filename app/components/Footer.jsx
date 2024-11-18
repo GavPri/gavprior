f63d1a27add19de5827bb3ea71918a82fbcc5674
德华 刘 <gavinprior5@gmail.com>
2024-11-18 14:09:09 +0000

    add style and links to footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React from "react";
+import { FiGithub } from "react-icons/fi";
+import { MdOutlineCopyright } from "react-icons/md";
+import { SlSocialLinkedin } from "react-icons/sl";
+import { TfiEmail } from "react-icons/tfi";
 
 export default function () {
   return (
-    <section className='w-full h-32 flex justify-between'>
-        <p></p>
-        <ul></ul>
+    <section className="w-full h-16 mb-4 flex justify-between items-center">
+      <p className="text-mutedForeground inline-flex items-center">
+        <span>
+          <MdOutlineCopyright className="mr-2"/>
+        </span>{" "}
+        gavprior.com{" "}
+      </p>
+      <ul className="flex gap-6">
+            <a
+              target="_blank"
+              href="https://github.com/GavPri"
+              className="text-mutedForeground hover:text-foreground"
+            >
+              <FiGithub size={20} />
+            </a>
+            <a
+              target="_blank"
+              href="https://linkedin.com/in/gavpdev"
+              className="text-mutedForeground hover:text-foreground"
+            >
+              <SlSocialLinkedin size={20} />
+            </a>
+            <a
+              href="mailto:gavinprior13@example.com"
+              className="text-mutedForeground hover:text-foreground"
+            >
+              <TfiEmail size={20} />
+            </a>
+      </ul>
     </section>
-  )
+  );
 }
